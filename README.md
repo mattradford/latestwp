76cf76ff68fe08cd732909b58a1f5c53b22ea9f9
Matt Radford <matt@mattrad.uk>
2023-07-20 22:32:50 +0100

    Update API to HTTPS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# latestwp
-The latest version of WordPress
+# The latest version of WordPress
+
+This website uses some frankly poor JavaScript to get the latest stable version of WordPress from the API at http://api.wordpress.org.
--- latest.js
+++ latest.js
@@ -1,7 +1,7 @@
 "use strict";
 
 async function showLatest() {
-    const response = await fetch("http://api.wordpress.org/core/stable-check/1.0/");
+    const response = await fetch("https://api.wordpress.org/core/stable-check/1.0/");
     const versions = await response.json();
     const versionsStr = JSON.stringify(versions);
     const indexOfLatest = versionsStr.indexOf('latest');
